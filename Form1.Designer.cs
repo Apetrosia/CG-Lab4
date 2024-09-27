31dab5235b320d717b2b48e6a03873835e7c7e8f
Анна Петросян <apetrosia@sfedu.ru>
2024-09-28 00:44:58 +0300

    Add drawing polygons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,64 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.CurrColorPB = new System.Windows.Forms.PictureBox();
+            this.NewPolygon = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrColorPB)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // pictureBox
             // 
-            this.button1.Location = new System.Drawing.Point(13, 14);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 65);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Задание 1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(3, 61);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1146, 480);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
             // 
-            // button2
+            // ClearButton
             // 
-            this.button2.Location = new System.Drawing.Point(167, 14);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(146, 65);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Задание 2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.ClearButton.Location = new System.Drawing.Point(12, 12);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(96, 43);
+            this.ClearButton.TabIndex = 1;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
             // 
-            // button3
+            // CurrColorPB
             // 
-            this.button3.Location = new System.Drawing.Point(321, 14);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(146, 65);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Задание 3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.CurrColorPB.Location = new System.Drawing.Point(114, 12);
+            this.CurrColorPB.Name = "CurrColorPB";
+            this.CurrColorPB.Size = new System.Drawing.Size(43, 43);
+            this.CurrColorPB.TabIndex = 2;
+            this.CurrColorPB.TabStop = false;
+            // 
+            // NewPolygon
+            // 
+            this.NewPolygon.Location = new System.Drawing.Point(163, 12);
+            this.NewPolygon.Name = "NewPolygon";
+            this.NewPolygon.Size = new System.Drawing.Size(143, 43);
+            this.NewPolygon.TabIndex = 3;
+            this.NewPolygon.Text = "новый полигон";
+            this.NewPolygon.UseVisualStyleBackColor = true;
+            this.NewPolygon.Click += new System.EventHandler(this.NewPolygon_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 94);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1151, 542);
+            this.Controls.Add(this.NewPolygon);
+            this.Controls.Add(this.CurrColorPB);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.pictureBox);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrColorPB)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.PictureBox CurrColorPB;
+        private System.Windows.Forms.Button NewPolygon;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,31 +1,161 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace CG_Lab
 {
     public partial class Form1 : Form
     {
+        List<Polygon> polygons;
+        Color currColor = Color.Black;
+        int currPolygonInd = 0;
+
         public Form1()
         {
             InitializeComponent();
+            Bitmap PictureBoxClear = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (Graphics g = Graphics.FromImage(PictureBoxClear))
+                g.Clear(Color.White);
+            pictureBox.Image = PictureBoxClear;
+            polygons = new List<Polygon>();
+            polygons.Add(new Polygon());
+            CurrColorPB.BackColor = currColor;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ClearButton_Click(object sender, EventArgs e)
         {
-            FormTask1 task1Form = new FormTask1();
-            task1Form.Show();
+            Bitmap PictureBoxClear = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (Graphics g = Graphics.FromImage(PictureBoxClear))
+                g.Clear(Color.White);
+            pictureBox.Image = PictureBoxClear;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void NewPolygon_Click(object sender, EventArgs e)
         {
-            FormTask2 task2Form = new FormTask2();
-            task2Form.Show();
+            if (polygons.Count > currPolygonInd)
+            {
+                int len = polygons[currPolygonInd].points.Count;
+                if (len > 2)
+                    DrawLineVu(polygons[currPolygonInd].points[len - 1], polygons[currPolygonInd].points[0]);
+                currPolygonInd++;
+            }
+            Random r = new Random();
+            currColor = Color.FromArgb(r.Next(100, 256), r.Next(0, 256), r.Next(0, 256), r.Next(0, 256));
+            CurrColorPB.BackColor = currColor;
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
         {
-            FormTask3 task2Form = new FormTask3();
-            task2Form.Show();
+            if (polygons.Count == currPolygonInd)
+                polygons.Add(new Polygon());
+            polygons[currPolygonInd].points.Add(new Point(e.Location.X, e.Location.Y));
+            int len = polygons[currPolygonInd].GetPointCount();
+            if (len > 1)
+                DrawLineVu(polygons[currPolygonInd].points[len - 2], polygons[currPolygonInd].points[len - 1]);
+
         }
+
+        private void DrawLineVu(Point p0, Point p1)
+        {
+            int x0 = p0.X;
+            int x1 = p1.X;
+            int y0 = p0.Y;
+            int y1 = p1.Y;
+
+            Bitmap pb = new Bitmap(pictureBox.Image);
+
+            pb.SetPixel(x1, y1, Color.FromArgb(255, 0, 0, 0));
+
+            float deltaX = x1 - x0;
+            float deltaY = y1 - y0;
+            float m = Math.Abs(deltaY / deltaX);
+
+            if (m <= 1)
+            {
+                float gradient = deltaY / deltaX;
+                if (x0 <= x1)
+                {
+                    float y = y0 + gradient;
+                    for (int x = x0 + 1; x <= x1; x++)
+                    {
+                        int alpha = (int)((1 - (y - (int)y)) * 255);
+                        pb.SetPixel(x, (int)y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        alpha = (int)((y - (int)y) * 255);
+                        pb.SetPixel(x, (int)y + 1, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        y += gradient;
+                    }
+                }
+                else
+                {
+                    gradient *= -1;
+                    float y = y0 + gradient;
+                    for (int x = x0 - 1; x >= x1; x--)
+                    {
+                        int alpha = (int)((1 - (y - (int)y)) * 255);
+                        pb.SetPixel(x, (int)y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        alpha = (int)((y - (int)y) * 255);
+                        pb.SetPixel(x, (int)y + 1, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        y += gradient;
+                    }
+                }
+            }
+            else
+            {
+                float gradient = deltaX / deltaY;
+                if (y0 <= y1)
+                {
+                    float x = x0 + gradient;
+                    for (int y = y0 + 1; y <= y1; y++)
+                    {
+                        int alpha = (int)((1 - (x - (int)x)) * 255);
+                        pb.SetPixel((int)x, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        alpha = (int)((x - (int)x) * 255);
+                        pb.SetPixel((int)x + 1, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        x += gradient;
+                    }
+                }
+                else
+                {
+                    gradient *= -1;
+                    float x = x0 + gradient;
+                    for (int y = y0 - 1; y >= y1; y--)
+                    {
+                        int alpha = (int)((1 - (x - (int)x)) * 255);
+                        pb.SetPixel((int)x, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        alpha = (int)((x - (int)x) * 255);
+                        pb.SetPixel((int)x + 1, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        x += gradient;
+                    }
+                }
+            }
+            pictureBox.Image = pb;
+        }
+    }
+
+    public class Polygon
+    {
+        public List<Point> points;
+
+        public Polygon() => points = new List<Point>();
+
+        public int GetPointCount() => points.Count;
     }
 }
