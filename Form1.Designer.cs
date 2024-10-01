cce3b7ac7d01eb74372f2678efea78c3951430b1
Daria Kolbasina <kolbasina9a@gmail.com>
2024-10-01 22:22:40 +0300

    Add vector algorithms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
+            this.searchingBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.CurrColorPB)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
@@ -50,20 +51,20 @@
             // pictureBox
             // 
             this.pictureBox.BackColor = System.Drawing.Color.White;
-            this.pictureBox.Location = new System.Drawing.Point(2, 40);
-            this.pictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox.Location = new System.Drawing.Point(3, 49);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(944, 585);
+            this.pictureBox.Size = new System.Drawing.Size(1259, 720);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
             // 
             // ClearButton
             // 
-            this.ClearButton.Location = new System.Drawing.Point(8, 8);
-            this.ClearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ClearButton.Location = new System.Drawing.Point(11, 10);
+            this.ClearButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(64, 28);
+            this.ClearButton.Size = new System.Drawing.Size(85, 34);
             this.ClearButton.TabIndex = 1;
             this.ClearButton.Text = "Очистить";
             this.ClearButton.UseVisualStyleBackColor = true;
@@ -71,19 +72,19 @@
             // 
             // CurrColorPB
             // 
-            this.CurrColorPB.Location = new System.Drawing.Point(76, 8);
-            this.CurrColorPB.Margin = new System.Windows.Forms.Padding(2);
+            this.CurrColorPB.Location = new System.Drawing.Point(101, 10);
+            this.CurrColorPB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CurrColorPB.Name = "CurrColorPB";
-            this.CurrColorPB.Size = new System.Drawing.Size(29, 28);
+            this.CurrColorPB.Size = new System.Drawing.Size(39, 34);
             this.CurrColorPB.TabIndex = 2;
             this.CurrColorPB.TabStop = false;
             // 
             // NewPolygon
             // 
-            this.NewPolygon.Location = new System.Drawing.Point(109, 8);
-            this.NewPolygon.Margin = new System.Windows.Forms.Padding(2);
+            this.NewPolygon.Location = new System.Drawing.Point(145, 10);
+            this.NewPolygon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NewPolygon.Name = "NewPolygon";
-            this.NewPolygon.Size = new System.Drawing.Size(95, 28);
+            this.NewPolygon.Size = new System.Drawing.Size(127, 34);
             this.NewPolygon.TabIndex = 3;
             this.NewPolygon.Text = "новый полигон";
             this.NewPolygon.UseVisualStyleBackColor = true;
@@ -92,10 +93,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(208, -1);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(277, -1);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.Size = new System.Drawing.Size(182, 16);
             this.label1.TabIndex = 5;
             this.label1.Text = "Афинные преобраозвания";
             // 
@@ -108,19 +108,19 @@
             "Поворот вокруг своего центра",
             "Масштабирование относительно заданной точки",
             "Масштабирование относительно своего центра"});
-            this.transformBox.Location = new System.Drawing.Point(208, 14);
-            this.transformBox.Margin = new System.Windows.Forms.Padding(2);
+            this.transformBox.Location = new System.Drawing.Point(277, 17);
+            this.transformBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.transformBox.Name = "transformBox";
-            this.transformBox.Size = new System.Drawing.Size(275, 21);
+            this.transformBox.Size = new System.Drawing.Size(365, 24);
             this.transformBox.TabIndex = 6;
             this.transformBox.Text = "Смещение на dx, dy";
             // 
             // transformButton
             // 
-            this.transformButton.Location = new System.Drawing.Point(486, 8);
-            this.transformButton.Margin = new System.Windows.Forms.Padding(2);
+            this.transformButton.Location = new System.Drawing.Point(648, 10);
+            this.transformButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.transformButton.Name = "transformButton";
-            this.transformButton.Size = new System.Drawing.Size(73, 28);
+            this.transformButton.Size = new System.Drawing.Size(97, 34);
             this.transformButton.TabIndex = 7;
             this.transformButton.Text = "Применить";
             this.transformButton.UseVisualStyleBackColor = true;
@@ -129,68 +129,89 @@
             // numericUpDown1
             // 
             this.numericUpDown1.DecimalPlaces = 1;
-            this.numericUpDown1.Location = new System.Drawing.Point(798, 89);
+            this.numericUpDown1.Location = new System.Drawing.Point(1064, 110);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             360,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 22);
             this.numericUpDown1.TabIndex = 9;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(814, 73);
+            this.label2.Location = new System.Drawing.Point(1085, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.Size = new System.Drawing.Size(119, 16);
             this.label2.TabIndex = 10;
             this.label2.Text = "Угол (в градусах)";
             // 
             // numericUpDown2
             // 
             this.numericUpDown2.DecimalPlaces = 2;
-            this.numericUpDown2.Location = new System.Drawing.Point(753, 244);
+            this.numericUpDown2.Location = new System.Drawing.Point(1004, 300);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             360,
             0,
             0,
             0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(77, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(103, 22);
             this.numericUpDown2.TabIndex = 11;
             // 
             // numericUpDown3
             // 
             this.numericUpDown3.DecimalPlaces = 2;
-            this.numericUpDown3.Location = new System.Drawing.Point(847, 244);
+            this.numericUpDown3.Location = new System.Drawing.Point(1129, 300);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown3.Maximum = new decimal(new int[] {
             360,
             0,
             0,
             0});
             this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown3.Size = new System.Drawing.Size(100, 22);
             this.numericUpDown3.TabIndex = 12;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(750, 228);
+            this.label3.Location = new System.Drawing.Point(1000, 281);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(181, 13);
+            this.label3.Size = new System.Drawing.Size(231, 16);
             this.label3.TabIndex = 13;
             this.label3.Text = "Коэффициенты масштабирования";
             // 
+            // searchingBox
+            // 
+            this.searchingBox.FormattingEnabled = true;
+            this.searchingBox.Items.AddRange(new object[] {
+            "Положение точки",
+            "Точка пересечения",
+            "Принадлежит ли точка полигону"});
+            this.searchingBox.Location = new System.Drawing.Point(838, 16);
+            this.searchingBox.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.searchingBox.Name = "searchingBox";
+            this.searchingBox.Size = new System.Drawing.Size(289, 24);
+            this.searchingBox.TabIndex = 10;
+            this.searchingBox.Text = "Векторные алгоритмы";
+            this.searchingBox.TextChanged += new System.EventHandler(this.searchingBox_TextChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(943, 624);
+            this.ClientSize = new System.Drawing.Size(1257, 768);
+            this.Controls.Add(this.searchingBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.numericUpDown2);
@@ -203,6 +224,7 @@
             this.Controls.Add(this.CurrColorPB);
             this.Controls.Add(this.ClearButton);
             this.Controls.Add(this.pictureBox);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
@@ -224,6 +246,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox transformBox;
         private System.Windows.Forms.Button transformButton;
+        private System.Windows.Forms.ComboBox searchingBox;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
