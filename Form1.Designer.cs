6759380aa2b0dfee257569b73398f8a0806babb0
AlexanderZemlyak <zemlyaksanya@mail.ru>
2024-10-01 13:40:47 +0300

    Add RotateCenter method (not working yet)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,25 +35,30 @@
             this.label1 = new System.Windows.Forms.Label();
             this.transformBox = new System.Windows.Forms.ComboBox();
             this.transformButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.CurrColorPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox
             // 
             this.pictureBox.BackColor = System.Drawing.Color.White;
-            this.pictureBox.Location = new System.Drawing.Point(3, 61);
+            this.pictureBox.Location = new System.Drawing.Point(2, 40);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1146, 480);
+            this.pictureBox.Size = new System.Drawing.Size(764, 312);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
             // 
             // ClearButton
             // 
-            this.ClearButton.Location = new System.Drawing.Point(12, 12);
+            this.ClearButton.Location = new System.Drawing.Point(8, 8);
+            this.ClearButton.Margin = new System.Windows.Forms.Padding(2);
             this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(96, 43);
+            this.ClearButton.Size = new System.Drawing.Size(64, 28);
             this.ClearButton.TabIndex = 1;
             this.ClearButton.Text = "Очистить";
             this.ClearButton.UseVisualStyleBackColor = true;
@@ -61,17 +66,19 @@
             // 
             // CurrColorPB
             // 
-            this.CurrColorPB.Location = new System.Drawing.Point(114, 12);
+            this.CurrColorPB.Location = new System.Drawing.Point(76, 8);
+            this.CurrColorPB.Margin = new System.Windows.Forms.Padding(2);
             this.CurrColorPB.Name = "CurrColorPB";
-            this.CurrColorPB.Size = new System.Drawing.Size(43, 43);
+            this.CurrColorPB.Size = new System.Drawing.Size(29, 28);
             this.CurrColorPB.TabIndex = 2;
             this.CurrColorPB.TabStop = false;
             // 
             // NewPolygon
             // 
-            this.NewPolygon.Location = new System.Drawing.Point(163, 12);
+            this.NewPolygon.Location = new System.Drawing.Point(109, 8);
+            this.NewPolygon.Margin = new System.Windows.Forms.Padding(2);
             this.NewPolygon.Name = "NewPolygon";
-            this.NewPolygon.Size = new System.Drawing.Size(143, 43);
+            this.NewPolygon.Size = new System.Drawing.Size(95, 28);
             this.NewPolygon.TabIndex = 3;
             this.NewPolygon.Text = "новый полигон";
             this.NewPolygon.UseVisualStyleBackColor = true;
@@ -80,9 +87,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(312, -1);
+            this.label1.Location = new System.Drawing.Point(208, -1);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(211, 20);
+            this.label1.Size = new System.Drawing.Size(141, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Афинные преобраозвания";
             // 
@@ -95,27 +103,49 @@
             "Поворот вокруг своего центра",
             "Масштабирование относительно заданной точки",
             "Масштабирование относительно своего центра"});
-            this.transformBox.Location = new System.Drawing.Point(312, 22);
+            this.transformBox.Location = new System.Drawing.Point(208, 14);
+            this.transformBox.Margin = new System.Windows.Forms.Padding(2);
             this.transformBox.Name = "transformBox";
-            this.transformBox.Size = new System.Drawing.Size(411, 28);
+            this.transformBox.Size = new System.Drawing.Size(275, 21);
             this.transformBox.TabIndex = 6;
             this.transformBox.Text = "Смещение на dx, dy";
             // 
             // transformButton
             // 
-            this.transformButton.Location = new System.Drawing.Point(729, 12);
+            this.transformButton.Location = new System.Drawing.Point(486, 8);
+            this.transformButton.Margin = new System.Windows.Forms.Padding(2);
             this.transformButton.Name = "transformButton";
-            this.transformButton.Size = new System.Drawing.Size(110, 43);
+            this.transformButton.Size = new System.Drawing.Size(73, 28);
             this.transformButton.TabIndex = 7;
             this.transformButton.Text = "Применить";
             this.transformButton.UseVisualStyleBackColor = true;
+            this.transformButton.Click += new System.EventHandler(this.transformButton_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(635, 91);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(651, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Угол (в градусах)";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(1151, 542);
+            this.ClientSize = new System.Drawing.Size(767, 352);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.transformButton);
             this.Controls.Add(this.transformBox);
             this.Controls.Add(this.label1);
@@ -123,11 +153,11 @@
             this.Controls.Add(this.CurrColorPB);
             this.Controls.Add(this.ClearButton);
             this.Controls.Add(this.pictureBox);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.CurrColorPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +172,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox transformBox;
         private System.Windows.Forms.Button transformButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Reflection;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
@@ -11,6 +13,10 @@ namespace CG_Lab
         List<Polygon> polygons;
         Color currColor = Color.Black;
         int currPolygonInd = 0;
+
+        private Action selectedAction;
+
+        private enum Action { Move, Rotation1, Rotation2, Scaling1, Scaling2 };
 
         public Form1()
         {
@@ -39,7 +45,7 @@ namespace CG_Lab
             {
                 int len = polygons[currPolygonInd].points.Count;
                 if (len > 2)
-                    DrawLineVu(polygons[currPolygonInd].points[len - 1], polygons[currPolygonInd].points[0]);
+                    MyGraphics.DrawLineVu(pictureBox, currColor, polygons[currPolygonInd].points[len - 1], polygons[currPolygonInd].points[0]);
                 currPolygonInd++;
             }
             Random r = new Random();
@@ -54,11 +60,166 @@ namespace CG_Lab
             polygons[currPolygonInd].points.Add(new Point(e.Location.X, e.Location.Y));
             int len = polygons[currPolygonInd].GetPointCount();
             if (len > 1)
-                DrawLineVu(polygons[currPolygonInd].points[len - 2], polygons[currPolygonInd].points[len - 1]);
+                MyGraphics.DrawLineVu(pictureBox, currColor, polygons[currPolygonInd].points[len - 2], polygons[currPolygonInd].points[len - 1]);
 
         }
 
-        private void DrawLineVu(Point p0, Point p1)
+        private void transformButton_Click(object sender, EventArgs e)
+        {
+            switch (transformBox.SelectedIndex)
+            {
+                case (int)Action.Move:
+
+                    break;
+                case (int)Action.Rotation1:
+
+                    break;
+                case (int)Action.Rotation2:
+                    RotateCenter();
+                    break;
+                case (int)Action.Scaling1:
+
+                    break;
+                case (int)Action.Scaling2:
+
+                    break;
+                default:
+                    MessageBox.Show("Не реализованная операция!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        private void RotateCenter()
+        {
+            int angle = (int)numericUpDown1.Value;
+
+            polygons.ForEach(poly =>
+            
+            {
+                Point center = poly.GetCenter();
+                MyGraphics.ClearPolygon(pictureBox, poly.points);
+
+                Matrix translationMatrix = new Matrix( new int[,]
+                    {
+                    { 1, 0, -center.X },
+                    { 0, 1, -center.Y },
+                    { 0, 0, 1 }
+                    });
+
+                for (int i = 0; i < poly.points.Count; i++)
+                {
+                    
+                        Matrix pointMatrix = new Matrix(new int[,]
+                        {
+                        { poly.points[i].X },
+                        { poly.points[i].Y },
+                        { 1 }
+                        });
+                        
+                        Matrix newPointMatrix = pointMatrix * translationMatrix;
+
+                        poly.points[i] = new Point(newPointMatrix[0, 0], newPointMatrix[1, 0]);
+                    
+                }
+
+                MyGraphics.DrawPolygon(pictureBox, currColor, poly.points);
+            }
+
+            );
+
+            pictureBox.Refresh();
+        }
+
+    }
+
+    public class Polygon
+    {
+        public List<Point> points;
+
+        public Polygon() => points = new List<Point>();
+
+        public int GetPointCount() => points.Count;
+
+        public Point GetCenter()
+        {
+            float x = 0, y = 0;
+            for (int i = 0; i < points.Count; i++)
+            {
+                x += points[i].X;
+                y += points[i].Y;
+            }
+            return new Point((int)Math.Round(x / points.Count), (int)Math.Round(y / points.Count));
+        }
+    }
+
+    public class Matrix
+    {
+        public int[,] Values { get; }
+
+        public Matrix(int[,] values)
+        {
+            Values = values;
+        }
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            int rowsA = A.Values.GetLength(0);
+            int colsA = A.Values.GetLength(1);
+            int rowsB = B.Values.GetLength(0);
+            int colsB = B.Values.GetLength(1);
+
+            int[,] result = new int[rowsA, colsB];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < colsA; k++)
+                    {
+                        result[i, j] += A.Values[i, k] * B.Values[k, j];
+                    }
+                }
+            }
+
+            return new Matrix(result);
+        }
+
+        public int this[int row, int column]
+        {
+            get
+            {
+                return Values[row, column];
+            }
+            set
+            {
+                Values[row, column] = value;
+            }
+        }
+    }
+
+    public static class MyGraphics
+    {
+
+        public static void ClearPolygon(PictureBox pictureBox, List<Point> points)
+        {
+            DrawPolygon(pictureBox, Color.White, points);
+        }
+
+        public static void DrawPolygon(PictureBox pictureBox, Color color, List<Point> points)
+        {
+            if (points.Count < 2)
+                return;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Point p0 = points[i];
+                Point p1 = points[(i + 1) % points.Count];
+                DrawLineVu(pictureBox, color, p0, p1);
+            }
+        }
+
+        public static void DrawLineVu(PictureBox pictureBox, Color color, Point p0, Point p1)
         {
             int x0 = p0.X;
             int x1 = p1.X;
@@ -82,13 +243,13 @@ namespace CG_Lab
                     for (int x = x0 + 1; x <= x1; x++)
                     {
                         int alpha = (int)((1 - (y - (int)y)) * 255);
-                        pb.SetPixel(x, (int)y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel(x, (int)y, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         alpha = (int)((y - (int)y) * 255);
-                        pb.SetPixel(x, (int)y + 1, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel(x, (int)y + 1, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         y += gradient;
                     }
                 }
@@ -99,13 +260,13 @@ namespace CG_Lab
                     for (int x = x0 - 1; x >= x1; x--)
                     {
                         int alpha = (int)((1 - (y - (int)y)) * 255);
-                        pb.SetPixel(x, (int)y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel(x, (int)y, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         alpha = (int)((y - (int)y) * 255);
-                        pb.SetPixel(x, (int)y + 1, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel(x, (int)y + 1, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         y += gradient;
                     }
                 }
@@ -119,13 +280,13 @@ namespace CG_Lab
                     for (int y = y0 + 1; y <= y1; y++)
                     {
                         int alpha = (int)((1 - (x - (int)x)) * 255);
-                        pb.SetPixel((int)x, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel((int)x, y, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         alpha = (int)((x - (int)x) * 255);
-                        pb.SetPixel((int)x + 1, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel((int)x + 1, y, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         x += gradient;
                     }
                 }
@@ -136,13 +297,13 @@ namespace CG_Lab
                     for (int y = y0 - 1; y >= y1; y--)
                     {
                         int alpha = (int)((1 - (x - (int)x)) * 255);
-                        pb.SetPixel((int)x, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel((int)x, y, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         alpha = (int)((x - (int)x) * 255);
-                        pb.SetPixel((int)x + 1, y, Color.FromArgb(255, currColor.R * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.G * alpha / 255 + (255 - alpha) * 255 / 255,
-                            currColor.B * alpha / 255 + (255 - alpha) * 255 / 255));
+                        pb.SetPixel((int)x + 1, y, Color.FromArgb(255, color.R * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.G * alpha / 255 + (255 - alpha) * 255 / 255,
+                            color.B * alpha / 255 + (255 - alpha) * 255 / 255));
                         x += gradient;
                     }
                 }
@@ -151,12 +312,4 @@ namespace CG_Lab
         }
     }
 
-    public class Polygon
-    {
-        public List<Point> points;
-
-        public Polygon() => points = new List<Point>();
-
-        public int GetPointCount() => points.Count;
-    }
 }
